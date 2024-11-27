1c68c1687f3ddb27d3cc89de7f1249327496f60d
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-11-27 17:43:21 +0100

    start retrieve abilities and their respective descriptions

--- src/fetchPoke.js
+++ src/fetchPoke.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export async function fetchPokemonData(idPoke) {
   const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+  const baseAbilityUrl = "https://pokeapi.co/api/v2/ability";
 
   try {
     const response = await axios.get(`${baseUrl}/${idPoke}`);
@@ -12,11 +13,27 @@ export async function fetchPokemonData(idPoke) {
     const height = pokemon.height;
     const baseExperience = pokemon.base_experience;
     const type = pokemon.types[0].type.name;
-    const abilities = pokemon.abilities.map((ability) => ability.ability.name);
+    // const abilities = pokemon.abilities.map((ability) => ability.ability.name);
 
+    const abilitiesAndDescription = await Promise.all (pokemon.abilities.map(async (ability) => {
+      const abilityReponse = await axios.get(`${baseAbilityUrl}/${idPoke}`);
+      const abilityData = abilityReponse.data;
 
+      const abilityName = abilityData.name;
+      const abilityDescription = abilityData.effect_entries[0].effect;
 
-    return {name, weight, height, baseExperience, type, abilities};
+      return {abilityName, abilityDescription}
+      
+    }));
+
+    return {
+      name,
+      weight,
+      height,
+      baseExperience,
+      type,
+      abilitiesAndDescription,
+    };
   } catch (error) {
     throw new Error(
       `Error during loading Pokémon with ID ${idPoke}: ${error.message}`
