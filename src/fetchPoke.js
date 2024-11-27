4c01973f4bbc62c274707c67ab4c1e458aacb630
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-11-27 16:52:23 +0100

    retrieve basic characteristics from JSON, fetchPoke.js modified

--- src/fetchPoke.js
+++ src/fetchPoke.js
@@ -1,18 +1,25 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 export async function fetchPokemonData(idPoke) {
-    const baseUrl = "https://pokeapi.co/api/v2/pokemon"
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon";
 
-    try {
-        const response = await axios.get(`${baseUrl}/${idPoke}`);
-        const pokemon = response.data;
+  try {
+    const response = await axios.get(`${baseUrl}/${idPoke}`);
+    const pokemon = response.data;
 
-        const name = pokemon.name;
+    const name = pokemon.name;
+    const weight = pokemon.weight;
+    const height = pokemon.height;
+    const baseExperience = pokemon.base_experience;
+    const type = pokemon.types[0].type.name;
 
-        return name;
-    } catch (error) {
-        console.error(`Error during loading Pokémon with ID ${idPoke}: ${error.message}`);
-        return null
-    }
+
+
+    return {name, weight, height, baseExperience, type};
+  } catch (error) {
+    console.error(
+      `Error during loading Pokémon with ID ${idPoke}: ${error.message}`
+    );
+    return null;
+  }
 }
