b415665937a422efc88c2ac7d33471ede42b2e6f
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-11-29 16:30:20 +0100

    deduplication occurred adding Set object in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import { fetchPokemonData } from './fetchPoke.js';
 
 async function fetchAllPokemons() {
   const pokemonsData = [];
+  const seenPokemon = new Set();
+
+
   for (let id = 1; id <= 151; id++) {
     const pokemon = await fetchPokemonData(id);
-    pokemonsData.push(pokemon);
+     if (!seenPokemon.has(pokemon.name)) {
+      seenPokemon.add(pokemon.name);
+      pokemonsData.push(pokemon);
+    } else {
+      console.log(`Pokémon ${pokemon.name} già visto, saltato.`);
+    }
   }
   console.log(pokemonsData);
 }
